8d2516a62b7b4e741b6bc511c8e81862ad6c3979
Шайхутдинов Амир <amchikshh@gmail.com>
2026-02-28 21:33:26 +0300

    Сделал еще несколько методов

--- src/CustomList.java
+++ src/CustomList.java
@@ -12,13 +12,14 @@ public class CustomList {
             addInTheEnd(array[i]);
         }
     }
-
+    // есть в мейне
     public void addInTheStart(int info){
         Node newNode = new Node(info);
         newNode.next = head;
         head = newNode;
     }
 
+    // есть в мейне
     public void addInTheEnd(int info){
         Node newNode = new Node(info);
         Node pointer = head;
@@ -53,6 +54,29 @@ public class CustomList {
         pointer.next = newNode;
     }
 
+    public void addRange(){
+
+    }
+
+    // есть в мейне
+    public void print(){
+        Node pointer = head;
+        if (head == null){
+            System.out.println("Список пуст");
+            return;
+        }
+
+        while(pointer!=null){
+            if (pointer.next == null){
+                System.out.println(pointer.info);
+            } else {
+                System.out.println(pointer.info + " ->");
+            }
+            pointer = pointer.next;
+        }
+    }
+
+    // есть в мейне
     public void deleteLast(){
         Node pointer = head;
 
@@ -71,6 +95,8 @@ public class CustomList {
         pointer.next = null;
     }
 
+    public void deleteIndex(){}
+
     public void deleteFirst(){
         if (head == null){
             System.out.println("Удаление невозможно");
@@ -78,5 +104,84 @@ public class CustomList {
         }
 
         head = head.next;
+    }
+
+    // есть в мейне
+    public void reverse(){
+        CustomList newList = new CustomList();
+        Node pointer = head;
+
+        if (pointer == null){
+            System.out.println("Список пуст");
+            return;
+        }
+
+        while(pointer!=null){
+            newList.addInTheStart(pointer.info);
+            pointer = pointer.next;
+        }
+        newList.print();
+    }
+
+    // есть в мейне
+    public int size(){
+        int counter = 0;
+        Node pointer = head;
+
+        if (head == null){
+            return counter;
+        }
+
+        while (pointer!= null){
+            counter++;
+            pointer = pointer.next;
+        }
+        return counter;
+    }
+
+    // есть в мейне
+    public boolean isEmpty(){
+        if (head == null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // есть в мейне
+    public boolean elementEntry(int info){
+        Node pointer = head;
+
+        if (pointer == null){
+            return false;
+        }
+
+        while (pointer != null){
+            if(pointer.info == info){
+                return true;
+            }
+            pointer = pointer.next;
+        }
+        return false;
+    }
+
+    // есть в мейне
+    public void elementEntryIndexes(int info){
+        Node pointer = head;
+        int index = 0;
+        CustomList indexesList = new CustomList();
+
+        if (pointer == null){
+            System.out.println("Список пуст");
+        }
+
+        while (pointer != null){
+            if (pointer.info == info) {
+                indexesList.addInTheEnd(index);
+            }
+            index++;
+            pointer = pointer.next;
+        }
+        indexesList.print();
     }
 }
